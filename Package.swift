130f3079f26cbc73af535c50118faea679e1f538
Algashev Alexander <algashev@mail.ru>
2025-11-28 16:19:21 +0300

    AVPlayerKit 1.3.0 -> 1.4.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SwiftExtensions/AVPlayerKit", from: "1.3.0"),
+        .package(url: "https://github.com/SwiftExtensions/AVPlayerKit", from: "1.4.0"),
+//        .package(url: "https://github.com/SwiftExtensions/AVPlayerKit", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
